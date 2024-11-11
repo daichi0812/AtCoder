a29679be3b13ee903b16538d057aee96d50dd4ab
Daichi <a5822108@aoyama.jp>
2024-11-11 19:25:00 +0900

    1: init

---
+++ ABC083/B-some_sums.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    int A;
+    int B;
+
+    cin >> N >> A >> B;
+
+    int total = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        int sum = 0;
+        int n = i;
+
+        while (n > 0)
+        {
+            sum += n % 10;
+            n = n / 10;
+        }
+        
+        if(sum >= A && sum <= B){
+            total += i;
+        }
+    }
+
+    cout << total << endl;
+
+}
