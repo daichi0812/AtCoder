45b3e6eac217e924f67343cfa81fd7381ba2bb4f
Daichi <a5822108@aoyama.jp>
2024-12-17 13:15:32 +0900

    ABC216-A問題

---
+++ ABC216/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    int y = s.at(n - 1) - '0';
+
+    s = s.substr(0, n - 2);
+
+    if(y <= 2){
+        s += '-';
+    }else if(y >= 7){
+        s += '+';
+    }
+
+    cout << s << endl;
+    return 0;
+}
