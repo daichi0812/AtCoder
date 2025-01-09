6ab3c56025df4711157573fb63c4fd9c2be82b6e
Daichi <a5822108@aoyama.jp>
2025-01-09 21:14:24 +0900

    24: ABC220-A問題

---
+++ ABC220/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int i = 1;
+    while (i < 100)
+    {
+        c = c * i;
+        if (a <= c && c <= b)
+        {
+            cout << c << endl;
+            return 0;
+        }
+
+        ++i;
+    }
+
+    cout << -1 << endl;
+}
