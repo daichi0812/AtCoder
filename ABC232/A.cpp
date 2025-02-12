96a53c604cdec12b1b1c352b6d8111afb2f75447
Daichi <a5822108@aoyama.jp>
2025-02-12 13:29:24 +0900

    51: ABC232-A問題

---
+++ ABC232/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int a = s.at(0) - '0';
+    int b = s.at(2) - '0';
+
+    cout << a * b << endl;
+}
