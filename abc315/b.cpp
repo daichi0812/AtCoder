66d6b73569dfbe03dd8dab6dda545465f9a8a33f
uN1teD <levif1812@gmail.com>
2025-06-09 11:16:53 +0900

    150: abc315-b問題

---
+++ abc315/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+
+int main(){
+    int m;
+    cin >> m;
+    vector<int> d(m);
+    int totalDays = 0;
+    rep(i, m) {
+        cin >> d[i];
+        totalDays += d[i];
+    }
+    int half = totalDays / 2;
+    int a = 0;
+    int b = 0;
+    rep(i, m){
+        if(d[i] >= half){
+            a = i + 1;
+            b = half + 1;
+            break;
+        }
+        half -= d[i];
+    }
+    cout << a << " " << b << endl;
+}
